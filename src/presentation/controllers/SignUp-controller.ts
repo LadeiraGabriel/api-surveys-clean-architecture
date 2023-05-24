8725805ca60cc075c56a3ee977613625af0eed5e
Gabriel <gabrielladeira8@gmail.com>
2023-05-24 17:23:30 -0300

    feat: Should return 400 if email not is provided

--- src/presentation/controllers/SignUp-controller.ts
+++ src/presentation/controllers/SignUp-controller.ts
@@ -1,8 +1,20 @@
 export class SignUpController {
   handle (request: any): any {
-    return {
-      statusCode: 400,
-      body: new Error('Missing param error!')
+    const { name, email } = request
+
+    if (!name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param error: name')
+
+      }
+    }
+
+    if (!email) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param error: email')
+      }
     }
   }
 }
