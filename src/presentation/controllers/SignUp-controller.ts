dcae1ec09f1a7044a6f8056ba698476ef6741c95
Gabriel <gabrielladeira8@gmail.com>
2023-05-25 17:31:32 -0300

    feat Should return 500 if EmailValidator throws

--- src/presentation/controllers/SignUp-controller.ts
+++ src/presentation/controllers/SignUp-controller.ts
@@ -6,19 +6,26 @@ import type { EmailValitor } from '../protocols/email-validator'
 import { InvalidParamError } from '../errors/Invalid-param-error'
 
 export class SignUpController implements Controller {
-  constructor (private readonly emailValidator: EmailValitor) {}
+  constructor (private readonly emailValidator: EmailValitor) { }
 
   handle (request: any): HttpResponse {
-    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-    const { email } = request
-    for (const field of requiredFields) {
-      if (!request[field]) {
-        return badRequest(new MissingParamError(field))
+    try {
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+      const { email } = request
+      for (const field of requiredFields) {
+        if (!request[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
-    }
-    const isValid = this.emailValidator.isValid(email)
-    if (!isValid) {
-      return badRequest(new InvalidParamError('email'))
+      const isValid = this.emailValidator.isValid(email)
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'))
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new Error()
+      }
     }
   }
 }
