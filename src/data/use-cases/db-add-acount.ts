8b160be0370785c09fbcf146986dd42a0899bf30
Gabriel <gabrielladeira8@gmail.com>
2023-05-29 17:55:34 -0300

    feat: Should call Hasher with correct value

---
+++ src/data/use-cases/db-add-acount.ts
@@ -0,0 +1,10 @@
+import type { AddAccount } from '../../domain/use-cases/account/add-account'
+import type { Hasher } from '../protocols/cryptography/Hasher'
+
+export class DbAddAccount implements AddAccount {
+  constructor (private readonly hasher: Hasher) { }
+  async add (account: AddAccount.Params): Promise<AddAccount.Result> {
+    await this.hasher.hash(account.password)
+    return Promise.resolve(true)
+  }
+}
