62ab5c57d9e16243532b6e3a4b66cdbf2a302739
Gabriel <gabrielladeira8@gmail.com>
2023-05-30 12:22:41 -0300

    test: should throw if hasher throw

--- tests/data/use-cases/account/db-add-acount.spec.ts
+++ tests/data/use-cases/account/db-add-acount.spec.ts
@@ -1,23 +1,35 @@
 import type { Hasher } from '../../../../src/data/protocols/cryptography/Hasher'
 import { DbAddAccount } from '../../../../src/data/use-cases/db-add-acount'
+import { HasherStub, mockFakeAccount } from '../../mocks/mocks-cryptography'
 
-class HasherStub implements Hasher {
-  async hash (value: string): Promise<string> {
-    return Promise.resolve('any_hash')
+type SutType = {
+  sut: DbAddAccount
+  hasherStub: Hasher
+}
+
+const makeSut = (): SutType => {
+  const hasherStub = new HasherStub()
+  const sut = new DbAddAccount(hasherStub)
+  return {
+    sut,
+    hasherStub
   }
 }
 
 describe('Db Add Account', () => {
   test('Should call Hasher with correct value', async () => {
-    const account = {
-      name: 'any_name',
-      email: 'any_email',
-      password: 'any_password'
-    }
-    const hasherStub = new HasherStub()
-    const sut = new DbAddAccount(hasherStub)
+    const account = mockFakeAccount()
+    const { sut, hasherStub } = makeSut()
     const hashSpy = jest.spyOn(hasherStub, 'hash')
     await sut.add(account)
     expect(hashSpy).toHaveBeenCalledWith(account.password)
+  })
+
+  test('Should throw if Hasher throw', async () => {
+    const account = mockFakeAccount()
+    const { sut, hasherStub } = makeSut()
+    jest.spyOn(hasherStub, 'hash').mockReturnValueOnce(Promise.reject(new Error()))
+    const promise = sut.add(account)
+    await expect(promise).rejects.toThrow()
   })
 })
