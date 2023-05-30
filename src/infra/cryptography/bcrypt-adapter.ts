210e53add1307f657fb49f7dca01b322bf59c693
Gabriel <gabrielladeira8@gmail.com>
2023-05-30 16:41:04 -0300

    feat: Should throws if bcrypt throws

--- src/infra/cryptography/bcrypt-adapter.ts
+++ src/infra/cryptography/bcrypt-adapter.ts
@@ -4,7 +4,7 @@ import bcrypt from 'bcrypt'
 export class BcryptAdapter implements Hasher {
   constructor (private readonly salt: number) { }
   async hash (value: string): Promise<string> {
-    await bcrypt.hash(value, this.salt)
-    return Promise.resolve('')
+    const hashed = await bcrypt.hash(value, this.salt)
+    return hashed
   }
 }
