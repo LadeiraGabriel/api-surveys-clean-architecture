4ccb0bc214014b83f13ff7cb459eede58949952c
Gabriel <gabrielladeira8@gmail.com>
2023-05-30 12:22:11 -0300

    feat: should throw if hasher throw

--- tests/data/mocks/mocks-cryptography.ts
+++ tests/data/mocks/mocks-cryptography.ts
@@ -1,5 +1,11 @@
 import type { Hasher } from '../../../src/data/protocols/cryptography/Hasher'
+import type { AddAccount } from '../../../src/domain/use-cases/account/add-account'
 
+export const mockFakeAccount = (): AddAccount.Params => ({
+  name: 'any_name',
+  email: 'any_email',
+  password: 'any_password'
+})
 export class HasherStub implements Hasher {
   async hash (value: string): Promise<string> {
     return Promise.resolve('any_hash')
