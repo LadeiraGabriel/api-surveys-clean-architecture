5d1c563a2ddc878d88be6eca5a8e4847a89aff85
Gabriel <gabrielladeira8@gmail.com>
2023-05-30 16:52:30 -0300

    refact: move sut to a factory method

--- tests/infra/cryptography/bcrypt-adapter.spec.ts
+++ tests/infra/cryptography/bcrypt-adapter.spec.ts
@@ -1,3 +1,4 @@
+import type { Hasher } from '../../../src/data/protocols/cryptography/Hasher'
 import { BcryptAdapter } from '../../../src/infra/cryptography/bcrypt-adapter'
 import bcrypt from 'bcrypt'
 
@@ -5,20 +6,32 @@ jest.mock('bcrypt', () => ({
   hash: () => 'any_hash'
 }))
 
+type SutType = {
+  sut: Hasher
+  salt: number
+}
+
+const makeSut = (): SutType => {
+  const salt = 12
+  const sut = new BcryptAdapter(salt)
+  return {
+    sut,
+    salt
+  }
+}
+
 describe('Bcrypt Adapter', () => {
   test('Should call bcrypt with correct values', async () => {
-    const salt = 12
     const value = 'any_value'
-    const sut = new BcryptAdapter(salt)
+    const { sut, salt } = makeSut()
     const spyHash = jest.spyOn(bcrypt, 'hash')
     await sut.hash(value)
     expect(spyHash).toHaveBeenCalledWith(value, salt)
   })
 
   test('Should throws if bcrypt throws', async () => {
-    const salt = 12
     const value = 'any_value'
-    const sut = new BcryptAdapter(salt)
+    const { sut } = makeSut()
     jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => {
       throw new Error()
     })
@@ -26,10 +39,9 @@ describe('Bcrypt Adapter', () => {
     await expect(promise).rejects.toThrow()
   })
 
-  test('Should return a hash success', async () => {
-    const salt = 12
+  test('Should return a hash on success', async () => {
     const value = 'any_value'
-    const sut = new BcryptAdapter(salt)
+    const { sut } = makeSut()
     const result = await sut.hash(value)
     expect(result).toEqual('any_hash')
   })
