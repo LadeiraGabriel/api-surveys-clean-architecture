3ae10dbc4491fc80b22acef62ecea7e96b997f28
Gabriel <gabrielladeira8@gmail.com>
2023-05-30 16:41:41 -0300

    test: Should throws if bcrypt throws

--- tests/infra/cryptography/bcrypt-adapter.spec.ts
+++ tests/infra/cryptography/bcrypt-adapter.spec.ts
@@ -1,6 +1,10 @@
 import { BcryptAdapter } from '../../../src/infra/cryptography/bcrypt-adapter'
 import bcrypt from 'bcrypt'
 
+jest.mock('bcrypt', () => ({
+  hash: () => 'any_hash'
+}))
+
 describe('Bcrypt Adapter', () => {
   test('Should call bcrypt with correct values', async () => {
     const salt = 12
@@ -9,5 +13,16 @@ describe('Bcrypt Adapter', () => {
     const spyHash = jest.spyOn(bcrypt, 'hash')
     await sut.hash(value)
     expect(spyHash).toHaveBeenCalledWith(value, salt)
+  })
+
+  test('Should throws if bcrypt throws', async () => {
+    const salt = 12
+    const value = 'any_value'
+    const sut = new BcryptAdapter(salt)
+    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const promise = sut.hash(value)
+    await expect(promise).rejects.toThrow()
   })
 })
