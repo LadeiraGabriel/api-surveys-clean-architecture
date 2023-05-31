2821d4a3952c19115135b73109c445bba7d24d93
Gabriel <gabrielladeira8@gmail.com>
2023-05-31 15:15:42 -0300

    test: Should call prisma with correct value

--- tests/infra/db/prisma/account-prisma-repository.spec.ts
+++ tests/infra/db/prisma/account-prisma-repository.spec.ts
@@ -1,19 +1,20 @@
+import { AccountPrismaRepository } from '../../../../src/infra/db/prisma/account-prisma-repository'
 import { prismaClientHelper } from '../../../../src/infra/helpers/prisma-client-helper'
 
 beforeAll(async () => {
-  await prismaClientHelper().account.deleteMany({})
+  await prismaClientHelper.account.deleteMany({})
 })
 describe('Account Prisma Repository', () => {
   describe('check account by email', () => {
     test('Should connect to the database create a user and return it through findUnique', async () => {
-      await prismaClientHelper().account.create({
+      await prismaClientHelper.account.create({
         data: {
           name: 'any_name',
           email: 'any_email',
           password: '$2b$12$0.L9KbPTZtGFz6C5kTpiN.MT8HmTyqpPMfAXxZi5CP9uGuWT45Upu'
         }
       })
-      const account = await prismaClientHelper().account.findUnique({
+      const account = await prismaClientHelper.account.findUnique({
         where: {
           email: 'any_email'
         }
@@ -29,6 +30,15 @@ describe('Account Prisma Repository', () => {
       expect(account.name).toEqual(result.name)
       expect(account.email).toEqual(result.email)
       expect(account.password).toEqual(result.password)
+    })
+
+    test('Should call prisma with correct value', async () => {
+      const sut = new AccountPrismaRepository()
+      const prismaSpy = jest.spyOn(prismaClientHelper.account, 'findUnique')
+      await sut.checkByEmail('any_email')
+      expect(prismaSpy).toHaveBeenCalledWith({
+        where: { email: 'any_email' }
+      })
     })
   })
 })
