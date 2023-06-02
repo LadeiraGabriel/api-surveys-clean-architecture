9c229f6ace03626ee0251876b1f9390dd81b8b22
Gabriel <gabrielladeira8@gmail.com>
2023-06-01 22:47:32 -0300

    feat: should call create with correct values

--- src/infra/db/prisma/account-prisma-repository.ts
+++ src/infra/db/prisma/account-prisma-repository.ts
@@ -1,7 +1,8 @@
 import type { CheckAccounByEmailRepository } from '../../../data/protocols/db/account/check-account-by-email-repository'
+import type { AddAccountRepository } from '../../../data/protocols/db/account/add-account-repository'
 import { prismaClientHelper } from '../../../../src/infra/helpers/prisma-client-helper'
 
-export class AccountPrismaRepository implements CheckAccounByEmailRepository {
+export class AccountPrismaRepository implements CheckAccounByEmailRepository, AddAccountRepository {
   async checkByEmail (email: CheckAccounByEmailRepository.Params): Promise<CheckAccounByEmailRepository.Result> {
     const findAccount = await prismaClientHelper.account.findUnique({
       where: {
@@ -9,5 +10,12 @@ export class AccountPrismaRepository implements CheckAccounByEmailRepository {
       }
     })
     return findAccount !== null
+  }
+
+  async add (account: AddAccountRepository.Params): Promise<AddAccountRepository.Result> {
+    await prismaClientHelper.account.create({
+      data: account
+    })
+    return Promise.resolve(true)
   }
 }
