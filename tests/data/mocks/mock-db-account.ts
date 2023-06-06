3425f01da1041c2e6b5a89fb666bbb3e46e92e20
Gabriel <gabrielladeira8@gmail.com>
2023-06-05 21:27:56 -0300

    refect: move loadAccountByrepositoryStub to mock files

--- tests/data/mocks/mock-db-account.ts
+++ tests/data/mocks/mock-db-account.ts
@@ -1,5 +1,6 @@
 import type { CheckAccounByEmailRepository } from '../../../src/data/protocols/db/account/check-account-by-email-repository'
 import type { AddAccountRepository } from '../../../src/data/protocols/db/account/add-account-repository'
+import type { LoadAccountByEmailRepository } from '../../../src/data/protocols/db/account/load-by-email-repository'
 
 export class CheckAccounByEmailRepositoryStub implements CheckAccounByEmailRepository {
   async checkByEmail (email: CheckAccounByEmailRepository.Params): Promise<CheckAccounByEmailRepository.Result> {
@@ -10,5 +11,17 @@ export class CheckAccounByEmailRepositoryStub implements CheckAccounByEmailRepos
 export class AddAccountRepositoryStub implements AddAccountRepository {
   async add (account: AddAccountRepository.Params): Promise<AddAccountRepository.Result> {
     return Promise.resolve(true)
+  }
+}
+
+export class LoadAccountByEmailRepositoryStub implements LoadAccountByEmailRepository {
+  async loadByEmail (plainText: LoadAccountByEmailRepository.Params): Promise<LoadAccountByEmailRepository.Result> {
+    return Promise.resolve({
+      id: 'any_id',
+      name: 'any_name',
+      email: 'any_email',
+      password: 'any_hash',
+      token: 'any_token'
+    })
   }
 }
--- tests/data/use-cases/db-authentication.spec.ts
+++ tests/data/use-cases/db-authentication.spec.ts
@@ -1,5 +1,6 @@
 import type { LoadAccountByEmailRepository } from '../../../src/data/protocols/db/account/load-by-email-repository'
 import { DbAthentication } from '../../../src/data/use-cases/db-authentication'
+import { LoadAccountByEmailRepositoryStub } from '../mocks/mock-db-account'
 
 /* receber um email e um password
     metodo para verificar se o email ja existe no banco
@@ -7,25 +8,28 @@ import { DbAthentication } from '../../../src/data/use-cases/db-authentication'
     gerar um token usando o id como base
     finalmente devolvendo o nome e o token para o usuario
     loadAccountByEmailRepostirory, HashCompare, Encrypter, updateAcessTokenRepository */
+
+type SutType = {
+  sut: DbAthentication
+  loadAccountByEmailRepositoryStub: LoadAccountByEmailRepository
+}
+
+const makeSut = (): SutType => {
+  const loadAccountByEmailRepositoryStub = new LoadAccountByEmailRepositoryStub()
+  const sut = new DbAthentication(loadAccountByEmailRepositoryStub)
+  return {
+    sut,
+    loadAccountByEmailRepositoryStub
+  }
+}
+
 describe('Db Authentication', () => {
   test('Should call loadAccountByEmailRepostirory with correct values', async () => {
-    class LoadAccountByEmailRepositoryStub implements LoadAccountByEmailRepository {
-      async loadByEmail (plainText: LoadAccountByEmailRepository.Params): Promise<LoadAccountByEmailRepository.Result> {
-        return Promise.resolve({
-          id: 'any_id',
-          name: 'any_name',
-          email: 'any_email',
-          password: 'any_hash',
-          token: 'any_token'
-        })
-      }
-    }
     const requiredFields = {
       email: 'any_email',
       password: 'any_password'
     }
-    const loadAccountByEmailRepositoryStub = new LoadAccountByEmailRepositoryStub()
-    const sut = new DbAthentication(loadAccountByEmailRepositoryStub)
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut()
     const loadByEmailSpy = jest.spyOn(loadAccountByEmailRepositoryStub, 'loadByEmail')
     await sut.auth(requiredFields)
     expect(loadByEmailSpy).toHaveBeenCalledWith('any_email')
