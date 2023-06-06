48bd5cbc938a6f9967fb5f2622bab4cbccd7cc9d
Gabriel <gabrielladeira8@gmail.com>
2023-06-05 22:15:48 -0300

    feat: should return null if hashCompare return false

--- src/data/use-cases/db-authentication.ts
+++ src/data/use-cases/db-authentication.ts
@@ -7,8 +7,12 @@ export class DbAthentication implements Authentication {
   async auth (data: Authentication.Params): Promise<Authentication.Result> {
     const account = await this.loadAccountByEmailRepository.loadByEmail(data.email)
     if (account) {
-      await this.hashComparer.compare(data.password, account.password)
-      return Promise.resolve(null)
+      const compareMatch = await this.hashComparer.compare(data.password, account.password)
+
+      if (compareMatch) {
+        return Promise.resolve(null)
+      }
+      return null
     }
     return null
   }
