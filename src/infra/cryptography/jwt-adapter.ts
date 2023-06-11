49d87a060964d6caad774b0a33c5203eaa40be9f
Gabriel <gabrielladeira8@gmail.com>
2023-06-11 20:47:29 -0300

    feat: should return token on success

--- src/infra/cryptography/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter.ts
@@ -4,7 +4,7 @@ import Jwt from 'jsonwebtoken'
 export class JwtAdapter implements Encrypter {
   constructor (private readonly secret: string) { }
   async encrypt (plainText: string): Promise<string> {
-    Jwt.sign({ id: plainText }, this.secret)
-    return Promise.resolve(null)
+    const token = Jwt.sign({ id: plainText }, this.secret)
+    return token
   }
 }
