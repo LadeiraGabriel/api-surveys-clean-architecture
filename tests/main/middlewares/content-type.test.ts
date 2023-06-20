88b5bf648cd9a59b7e49354cfc4dfdbf5f419edf
Gabriel <gabrielladeira8@gmail.com>
2023-06-20 20:44:52 -0300

    test: should have content type xml

--- tests/main/middlewares/content-type.test.ts
+++ tests/main/middlewares/content-type.test.ts
@@ -7,10 +7,18 @@ describe('Content type', () => {
   beforeAll(async () => {
     app = await setupApp()
   })
-  test('Should have content type json', async () => {
+  test('Should have content type json default', async () => {
     app.get('/test-content-type', (req: Request, res: Response) => {
       res.send('')
     })
     await request(app).get('/test-content-type').expect('content-type', /json/)
+  })
+
+  test('Should have content type xml', async () => {
+    app.get('/test-content-type-xml', (req: Request, res: Response) => {
+      res.type('xml')
+      res.send('')
+    })
+    await request(app).get('/test-content-type-xml').expect('content-type', /xml/)
   })
 })
