67048e554e48892b3d9377183471f5fd0a7b94d7
Gabriel <gabrielladeira8@gmail.com>
2023-06-20 21:30:54 -0300

    feat: add routes to access endpoints

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import type { Express } from 'express'
 import setupMiddleware from './middleware'
+import setupRoutes from './router'
 import express from 'express'
 export const setupApp = async (): Promise<Express> => {
   const app = express()
   setupMiddleware(app)
+  setupRoutes(app)
   return app
 }
---
+++ src/main/routes/login-routes.ts
@@ -0,0 +1,7 @@
+import type { Router } from 'express'
+
+export default (router: Router): void => {
+  router.post('/sign-up', (req, res) => {
+    res.json({ ok: 'ok' })
+  })
+}
