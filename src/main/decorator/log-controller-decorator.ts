f911dff8a1c93d0868edd761b1fdd54b61367b5a
Gabriel <gabrielladeira8@gmail.com>
2023-06-25 14:20:15 -0300

    feat: should call logErrorRepository if return controller return 500

--- src/main/decorator/log-controller-decorator.ts
+++ src/main/decorator/log-controller-decorator.ts
@@ -1,9 +1,15 @@
+import type { LogErrorRepository } from './../../data/protocols/db/log/log-error-repository'
 import type { Controller, HttpResponse } from '../../presentation/protocols'
 
 export class LogControllerDecorator implements Controller {
-  constructor (private readonly controller: Controller) { }
+  constructor (private readonly controller: Controller, private readonly logErrorRepository: LogErrorRepository) { }
   async handle (request: any): Promise<HttpResponse> {
-    const response = await this.controller.handle(request)
-    return response
+    const httpResponse = await this.controller.handle(request)
+
+    if (httpResponse.statusCode === 500) {
+      await this.logErrorRepository.logError(httpResponse.body.stack)
+    }
+
+    return httpResponse
   }
 }
--- tests/main/mocks/mock-controller.ts
+++ tests/main/mocks/mock-controller.ts
@@ -1,7 +1,8 @@
+import { ok } from '../../../src/presentation/helpers/http-helper'
 import type { Controller, HttpResponse } from '../../../src/presentation/protocols'
 
 export class ControllerStub implements Controller {
   async handle (request: any): Promise<HttpResponse> {
-    return Promise.resolve(null)
+    return ok('any_data')
   }
 }
