ae497c069e288acd52c0892ae2230e45dbdbc9dc
Gabriel <gabrielladeira8@gmail.com>
2023-06-25 14:20:36 -0300

    test: should call logErrorRepository if return controller return 500

--- tests/main/decorator/log-controller-decorator.spec.ts
+++ tests/main/decorator/log-controller-decorator.spec.ts
@@ -1,14 +1,24 @@
+import type { LogErrorRepository } from '../../../src/data/protocols/db/log/log-error-repository'
 import { LogControllerDecorator } from '../../../src/main/decorator/log-controller-decorator'
+import { serverError } from '../../../src/presentation/helpers/http-helper'
 import { ControllerStub } from '../mocks/mock-controller'
 
 type SutType = {
+  logErrorRepositoryStub: LogErrorRepository
   controller: ControllerStub
   sut: LogControllerDecorator
 }
 const makeSut = (): SutType => {
+  class LogErrorRepositoryStub implements LogErrorRepository {
+    async logError (stack: string): Promise<void> {
+      return Promise.resolve(null)
+    }
+  }
+  const logErrorRepositoryStub = new LogErrorRepositoryStub()
   const controller = new ControllerStub()
-  const sut = new LogControllerDecorator(controller)
+  const sut = new LogControllerDecorator(controller, logErrorRepositoryStub)
   return {
+    logErrorRepositoryStub,
     controller,
     sut
   }
@@ -26,5 +36,22 @@ describe('Log Decorator', () => {
     const handleSpy = jest.spyOn(controller, 'handle')
     await sut.handle(request)
     expect(handleSpy).toHaveBeenCalledWith(request)
+  })
+
+  test('should call logErrorRepository if return controller return 500', async () => {
+    const request = {
+      name: 'any_name',
+      email: 'any_email',
+      password: 'any_password',
+      passwordConfimation: 'any_password'
+    }
+    const error = new Error()
+    const { sut, controller, logErrorRepositoryStub } = makeSut()
+    jest.spyOn(controller, 'handle').mockImplementationOnce(async () => {
+      return serverError(error)
+    })
+    const logSpy = jest.spyOn(logErrorRepositoryStub, 'logError')
+    await sut.handle(request)
+    expect(logSpy).toHaveBeenCalledWith(error.stack)
   })
 })
