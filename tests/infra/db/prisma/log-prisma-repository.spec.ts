bdca183a294409833cf58268e7d29b7e24215b40
Gabriel <gabrielladeira8@gmail.com>
2023-06-26 21:00:11 -0300

    test: should create log on success

---
+++ tests/infra/db/prisma/log-prisma-repository.spec.ts
@@ -0,0 +1,22 @@
+import { LogPrismaRepository } from '../../../../src/infra/db/prisma/log-prisma-repository'
+import { prismaClientHelper } from '../../../../src/infra/helpers/prisma-client-helper'
+
+describe('Log prisma repository', () => {
+  beforeEach(async () => {
+    await prismaClientHelper.log.deleteMany({})
+  })
+  afterEach(async () => {
+    await prismaClientHelper.log.deleteMany({})
+  })
+  test('Should create log on success', async () => {
+    const stack = 'any_error'
+    const sut = new LogPrismaRepository()
+    await sut.logError(stack)
+    const logs = await prismaClientHelper.log.findFirst({
+      where: {
+        stack
+      }
+    })
+    expect(logs.stack).toEqual(stack)
+  })
+})
