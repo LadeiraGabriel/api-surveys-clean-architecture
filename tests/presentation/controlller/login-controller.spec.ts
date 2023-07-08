867cab762ae6d65f14c0dec1693edb57b130afa5
Gabriel <gabrielladeira8@gmail.com>
2023-07-08 16:33:16 -0300

    refact: move sut to a factory method

--- tests/presentation/controlller/login-controller.spec.ts
+++ tests/presentation/controlller/login-controller.spec.ts
@@ -1,24 +1,29 @@
 import type { Authentication } from '../../../src/domain/use-cases'
 import { LoginController } from '../../../src/presentation/controllers/login-controller'
+import { mockAuthenticationStub } from '../mocks'
+
+type SutType = () => {
+  sut: LoginController
+  authenticationStub: Authentication
+}
+
+const makeSut: SutType = () => {
+  const authenticationStub = mockAuthenticationStub()
+  const sut = new LoginController(authenticationStub)
+  return {
+    sut,
+    authenticationStub
+  }
+}
 
 describe('Login controller', () => {
   test('Should call authentication with correct values', async () => {
-    class AuthenticationStub implements Authentication {
-      async auth (data: Authentication.Params): Promise<Authentication.Result> {
-        return Promise.resolve({
-          name: 'any_name',
-          acessToken: 'any_token'
-        })
-      }
-    }
     const request = {
       email: 'any_email',
       password: 'any_password'
     }
-
-    const authenticationStub = new AuthenticationStub()
+    const { sut, authenticationStub } = makeSut()
     const authSpy = jest.spyOn(authenticationStub, 'auth')
-    const sut = new LoginController(authenticationStub)
     await sut.handle(request)
     expect(authSpy).toBeCalledWith(request)
   })
