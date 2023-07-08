c03a8f2aaf4a4e333981b4909762eb05623a92a3
Gabriel <gabrielladeira8@gmail.com>
2023-07-08 17:43:26 -0300

    feat: should return 500 if authentication fails

--- src/presentation/controllers/login-controller.ts
+++ src/presentation/controllers/login-controller.ts
@@ -1,18 +1,21 @@
-import { unauthorized } from '../helpers/http-helper'
+import { serverError, unauthorized } from '../helpers/http-helper'
 import type { HttpResponse } from '../protocols'
 import type { Authentication } from './../../domain/use-cases/Authentication'
 import type { Controller } from './../protocols/controller'
 export class LoginController implements Controller {
   constructor (private readonly authentication: Authentication) {}
   async handle (request: any): Promise<HttpResponse> {
-    const isAuthaticate = await this.authentication.auth(request)
-
-    if (!isAuthaticate) {
-      return unauthorized()
+    try {
+      const isAuthaticate = await this.authentication.auth(request)
+      if (!isAuthaticate) {
+        return unauthorized()
+      }
+      return Promise.resolve({
+        statusCode: 200,
+        body: ''
+      })
+    } catch (e) {
+      return serverError(e)
     }
-    return Promise.resolve({
-      statusCode: 200,
-      body: ''
-    })
   }
 }
