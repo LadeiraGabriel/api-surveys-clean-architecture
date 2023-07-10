d87dcfbd31b5c05109516c538cf645732d32d4e1
Gabriel <gabrielladeira8@gmail.com>
2023-07-09 23:11:05 -0300

    test: should return 200 on success in route login

--- tests/main/routes/login-routes.test.ts
+++ tests/main/routes/login-routes.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest'
 import type { Express } from 'express'
 import { setupApp } from '../../../src/main/config/app'
+import { prismaClientHelper } from '../../../src/infra/helpers/prisma-client-helper'
 
 let app: Express
 
@@ -9,12 +10,38 @@ describe('Routes', () => {
     app = await setupApp()
   })
 
-  test('Should return 200 on success', async () => {
-    await request(app).post('/api/signup').send({
-      name: 'any_name',
-      email: 'any_email@gmail.com',
-      password: 'any_password',
-      passwordConfirmation: 'any_password'
-    }).expect(200)
+  beforeEach(async () => {
+    await prismaClientHelper.account.deleteMany({})
+  })
+  afterEach(async () => {
+    await prismaClientHelper.account.deleteMany({})
+  })
+
+  describe('sign up', () => {
+    test('Should return 200 on success', async () => {
+      await request(app).post('/api/signup').send({
+        name: 'any_name',
+        email: 'any_email@gmail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }).expect(200)
+    })
+  })
+
+  describe('Login', () => {
+    test('Should return 200 on success', async () => {
+      await prismaClientHelper.account.create({
+        data: {
+          name: 'any_name',
+          email: 'any_email@gmail.com',
+          password: '$2b$12$vE.nSc9qmclKapl15JxG/exfGpSHUzY2MCIjs/hLYlVNC3ITW.kt.'
+        }
+      })
+
+      await request(app).post('/api/login').send({
+        email: 'any_email@gmail.com',
+        password: 'any_password'
+      }).expect(200)
+    })
   })
 })
