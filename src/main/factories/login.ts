2ce70699b965847754020aa8eb74a718b6c31916
Gabriel <gabrielladeira8@gmail.com>
2023-07-09 23:10:46 -0300

    feat: should return 200 on success in route login

---
+++ src/main/factories/login.ts
@@ -0,0 +1,21 @@
+import { DbAthentication } from '../../data/use-cases/db-authentication'
+import { BcryptAdapter } from '../../infra/cryptography/bcrypt-adapter'
+import { JwtAdapter } from '../../infra/cryptography/jwt-adapter'
+import { AccountPrismaRepository } from '../../infra/db/prisma/account-prisma-repository'
+import { LogPrismaRepository } from '../../infra/db/prisma/log-prisma-repository'
+import { LoginController } from '../../presentation/controllers/login-controller'
+import type { Controller } from '../../presentation/protocols'
+import { EmailValidatorApdater } from '../../validations/validator/Email-validator-adapter'
+import { LogControllerDecorator } from '../decorator/log-controller-decorator'
+
+export const makeLoginController = (): Controller => {
+  const salt = 12
+  const emailValidatorAdapter = new EmailValidatorApdater()
+  const accountPrismaRepository = new AccountPrismaRepository()
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const jwtAdapter = new JwtAdapter(process.env.SECRET_KEY)
+  const dbAuthentication = new DbAthentication(accountPrismaRepository, bcryptAdapter, jwtAdapter, accountPrismaRepository)
+  const loginController = new LoginController(emailValidatorAdapter, dbAuthentication)
+  const logPrismaRepository = new LogPrismaRepository()
+  return new LogControllerDecorator(loginController, logPrismaRepository)
+}
--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,7 +1,9 @@
 import type { Router } from 'express'
 import { adapterRoute } from '../adapters/adapterRoute'
 import { makeSignUpController } from '../factories/signup'
+import { makeLoginController } from '../factories/login'
 
 export default (router: Router): void => {
   router.post('/signup', adapterRoute(makeSignUpController()))
+  router.post('/login', adapterRoute(makeLoginController()))
 }
