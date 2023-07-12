94f19840bd814d2dd06b7bf615b7400eae7f45ba
Gabriel <gabrielladeira8@gmail.com>
2023-07-12 20:26:28 -0300

    feat: should return error if email validator return false

--- src/validations/validators/email-validation.ts
+++ src/validations/validators/email-validation.ts
@@ -1,10 +1,13 @@
+import { InvalidParamError } from '../../presentation/errors'
 import type { Validation } from '../../presentation/protocols/validation'
 import type { EmailValitor } from '../protocols'
 
 export class EmailValidation implements Validation {
   constructor (private readonly emailValidator: EmailValitor) {}
   validate (input: any): Error {
-    this.emailValidator.isValid(input.email)
-    return new Error()
+    const emailIsValid = this.emailValidator.isValid(input.email)
+    if (!emailIsValid) {
+      return new InvalidParamError('email')
+    }
   }
 }
