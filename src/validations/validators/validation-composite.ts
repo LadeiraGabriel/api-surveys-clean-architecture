ea536188e7ce2d72d42f977708f52db5a3230823
Gabriel <gabrielladeira8@gmail.com>
2023-07-11 22:54:35 -0300

    feat: should return error if validation return error

--- src/validations/validators/validation-composite.ts
+++ src/validations/validators/validation-composite.ts
@@ -4,8 +4,10 @@ export class ValidationComposite implements Validation {
   constructor (private readonly validations: Validation[]) {}
   validate (input: any): Error {
     for (const validation of this.validations) {
-      validation.validate(input)
+      const error = validation.validate(input)
+      if (error) {
+        return error
+      }
     }
-    return null
   }
 }
