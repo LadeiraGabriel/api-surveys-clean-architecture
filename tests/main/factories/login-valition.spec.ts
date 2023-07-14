e6ff1e65eefb8d8ccadb2fa97cecba592ef831e9
Gabriel <gabrielladeira8@gmail.com>
2023-07-13 21:50:30 -0300

    test: should call validation composite with all validations

---
+++ tests/main/factories/login-valition.spec.ts
@@ -0,0 +1,19 @@
+import type { Validation } from '../../../src/presentation/protocols'
+import { ValidationComposite } from './../../../src/validations/validators/validation-composite'
+import { EmailValidatorApdater } from './../../../src/infra/validators/Email-validator-adapter'
+import { EmailValidation } from '../../../src/validations/validators/email-validation'
+import { RequiredFieldsValidation } from '../../../src/validations/validators/required-field-validation'
+import { makeLoginValidation } from '../../../src/main/factories/login-validation'
+
+jest.mock('./../../../src/validations/validators/validation-composite')
+describe('Login validation', () => {
+  test('Should call valitionComposite with all validations', () => {
+    makeLoginValidation()
+    const validations: Validation[] = [
+      new RequiredFieldsValidation('email'),
+      new RequiredFieldsValidation('password'),
+      new EmailValidation(new EmailValidatorApdater())
+    ]
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
