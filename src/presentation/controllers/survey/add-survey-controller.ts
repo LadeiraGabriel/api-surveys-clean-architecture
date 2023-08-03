b0f24f63d96c576887f81e4e0e07b35706650b91
Gabriel <gabrielladeira8@gmail.com>
2023-08-02 22:22:29 -0300

    feat: should return 400 if validation return a error

--- src/presentation/controllers/survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey-controller.ts
@@ -1,9 +1,13 @@
+import { badRequest } from '../../helpers/http-helper'
 import type { Controller, HttpResponse, Validation } from '../../protocols'
 
 export class AddSurveyController implements Controller {
   constructor (private readonly validation: Validation) { }
   async handle (request: any): Promise<HttpResponse> {
-    this.validation.validate(request)
+    const error = this.validation.validate(request)
+    if (error) {
+      return badRequest(error)
+    }
     return Promise.resolve({
       statusCode: 200,
       body: ''
