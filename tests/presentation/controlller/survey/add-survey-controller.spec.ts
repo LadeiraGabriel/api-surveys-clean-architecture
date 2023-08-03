5662867698629db5446105034ea7ebb856b1c181
Gabriel <gabrielladeira8@gmail.com>
2023-08-02 22:39:59 -0300

    test: should call addsurvey with correct values

--- tests/presentation/controlller/survey/add-survey-controller.spec.ts
+++ tests/presentation/controlller/survey/add-survey-controller.spec.ts
@@ -1,19 +1,24 @@
+import type { AddSurvey } from '../../../../src/domain/use-cases/add-survey'
 import { AddSurveyController } from '../../../../src/presentation/controllers/survey/add-survey-controller'
 import { badRequest } from '../../../../src/presentation/helpers/http-helper'
 import type { Controller, Validation } from '../../../../src/presentation/protocols'
 import { mockValidationStub } from '../../mocks'
+import { mockAddSurveyStub } from '../../mocks/mock-survey'
 
 type SutType = {
   sut: Controller
   validationStub: Validation
+  addSuveyStub: AddSurvey
 }
 
 const makeSut = (): SutType => {
   const validationStub = mockValidationStub()
-  const sut = new AddSurveyController(validationStub)
+  const addSuveyStub = mockAddSurveyStub()
+  const sut = new AddSurveyController(validationStub, addSuveyStub)
   return {
     sut,
-    validationStub
+    validationStub,
+    addSuveyStub
   }
 }
 
@@ -44,5 +49,19 @@ describe('Add Survey controller', () => {
     jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
     const httpResponse = await sut.handle(request)
     expect(httpResponse).toEqual(badRequest(new Error()))
+  })
+
+  test('should call add survey with correct values', async () => {
+    const request = {
+      question: 'any_question',
+      anwerns: {
+        anwerns: 'any_anwern',
+        image: 'any_image'
+      }
+    }
+    const { sut, addSuveyStub } = makeSut()
+    const spyAdd = jest.spyOn(addSuveyStub, 'add')
+    await sut.handle(request)
+    expect(spyAdd).toHaveBeenCalledWith(request)
   })
 })
