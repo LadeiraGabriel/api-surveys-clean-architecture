c53a628addbe3064c87247c40f613c6e8f384cf7
Gabriel <gabrielladeira8@gmail.com>
2023-08-03 19:11:38 -0300

    feat: create a factory method to the creation of validation composite of addSurvey

---
+++ src/main/factories/add-survey-validation.ts
@@ -0,0 +1,8 @@
+import { RequiredFieldsValidation, ValidationComposite } from '../../validations/validators'
+
+export const makeAddSurveyValidation = (): ValidationComposite => {
+  return new ValidationComposite([
+    new RequiredFieldsValidation('question'),
+    new RequiredFieldsValidation('anwers')
+  ])
+}
