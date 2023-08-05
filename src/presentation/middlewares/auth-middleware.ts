9d26966713efc18a9477b4cf31f7868514982383
Gabriel <gabrielladeira8@gmail.com>
2023-08-04 23:04:50 -0300

    feat: should call load account by token with correct values

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,10 +1,16 @@
+import type { LoadAccountByToken } from '../../domain/load-account-by-token'
 import { AccessDeniedError } from '../errors/access-denied-error'
 import { forbidden } from '../helpers/http-helper'
 import type { HttpResponse } from '../protocols'
 import type { Middleware } from '../protocols/middleware'
 
 export class AuthMiddleware implements Middleware {
+  constructor (private readonly loadAccountByToken: LoadAccountByToken, private readonly role?: string) { }
   async auth (request: any): Promise<HttpResponse> {
-    return Promise.resolve(forbidden(new AccessDeniedError()))
+    const { accessToken } = request
+    if (accessToken) {
+      await this.loadAccountByToken.load(accessToken, this.role)
+    }
+    return forbidden(new AccessDeniedError())
   }
 }
