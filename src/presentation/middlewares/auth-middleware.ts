96dace0021ab002f8d9393f8405dc9bfb91bc682
Gabriel <gabrielladeira8@gmail.com>
2023-08-05 11:21:17 -0300

    feat: should return 500 if load account by token throws

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,16 +1,20 @@
 import type { LoadAccountByToken } from '../../domain/load-account-by-token'
 import { AccessDeniedError } from '../errors/access-denied-error'
-import { forbidden } from '../helpers/http-helper'
+import { forbidden, serverError } from '../helpers/http-helper'
 import type { HttpResponse } from '../protocols'
 import type { Middleware } from '../protocols/middleware'
 
 export class AuthMiddleware implements Middleware {
   constructor (private readonly loadAccountByToken: LoadAccountByToken, private readonly role?: string) { }
   async auth (request: any): Promise<HttpResponse> {
-    const { accessToken } = request
-    if (accessToken) {
-      await this.loadAccountByToken.load(accessToken, this.role)
+    try {
+      const { accessToken } = request
+      if (accessToken) {
+        await this.loadAccountByToken.load(accessToken, this.role)
+      }
+      return forbidden(new AccessDeniedError())
+    } catch (error) {
+      return serverError(error)
     }
-    return forbidden(new AccessDeniedError())
   }
 }
