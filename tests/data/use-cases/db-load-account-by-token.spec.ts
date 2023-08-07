a24f42ecf31fdb393dfb864671294986d076b8ae
Gabriel <gabrielladeira8@gmail.com>
2023-08-06 21:41:25 -0300

    test: should call decrypter with acessToken

--- tests/data/use-cases/db-load-account-by-token.spec.ts
+++ tests/data/use-cases/db-load-account-by-token.spec.ts
@@ -1,23 +1,35 @@
+import type { Decrypter } from '../../../src/data/protocols/cryptography/descrypter'
 import type { LoadAccountByTokenRepository } from '../../../src/data/protocols/db/account/load-account-by-token-repository'
 import { DbLoadAccountByToken } from '../../../src/data/use-cases/db-load-account-by-token'
+import { mockdecrypterStub } from '../mocks/mock-cryptography'
 import { mockLoadAccountByTokenRepositoryStub } from '../mocks/mock-db-account'
 
 type SutType = {
   sut: DbLoadAccountByToken
   loadAccountByTokenRepositoryStub: LoadAccountByTokenRepository
-
+  decrypterStub: Decrypter
 }
 
 const makeSut = (): SutType => {
+  const decrypterStub = mockdecrypterStub()
   const loadAccountByTokenRepositoryStub = mockLoadAccountByTokenRepositoryStub()
-  const sut = new DbLoadAccountByToken(loadAccountByTokenRepositoryStub)
+  const sut = new DbLoadAccountByToken(loadAccountByTokenRepositoryStub, decrypterStub)
   return {
     sut,
-    loadAccountByTokenRepositoryStub
+    loadAccountByTokenRepositoryStub,
+    decrypterStub
   }
 }
 
 describe('Db load account by token use case', () => {
+  test('should call decrypter with access token', async () => {
+    const accessToken: string = 'any_token'
+    const role = 'any_role'
+    const { sut, decrypterStub } = makeSut()
+    const spyDecript = jest.spyOn(decrypterStub, 'decrypt')
+    await sut.load(accessToken, role)
+    expect(spyDecript).toHaveBeenCalledWith(accessToken)
+  })
   test('should call load account by token repository with correct values', async () => {
     const accessToken: string = 'any_token'
     const role = 'any_role'
