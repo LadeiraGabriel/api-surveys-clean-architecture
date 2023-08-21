21e6f499466b4d3580b664a6af0ce952c0646501
Gabriel <gabrielladeira8@gmail.com>
2023-08-20 21:32:23 -0300

    feat: should return null if decrypter throws

--- src/data/use-cases/db-load-account-by-token.ts
+++ src/data/use-cases/db-load-account-by-token.ts
@@ -5,7 +5,12 @@ import type { LoadAccountByTokenRepository } from '../protocols/db/account/load-
 export class DbLoadAccountByToken implements LoadAccountByToken {
   constructor (private readonly loadAccountByTokenRepository: LoadAccountByTokenRepository, private readonly decrypter: Decrypter) { }
   async load (accessToken: string, role?: string): Promise<LoadAccountByToken.Result> {
-    const token = await this.decrypter.decrypt(accessToken)
+    let token: string
+    try {
+      token = await this.decrypter.decrypt(accessToken)
+    } catch (error) {
+      return null
+    }
     if (token) {
       const accountId = await this.loadAccountByTokenRepository.loadAccountByToken(accessToken, role)
       if (accountId) {
