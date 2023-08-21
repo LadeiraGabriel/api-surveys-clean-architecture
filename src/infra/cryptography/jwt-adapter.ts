b9d5dae53aab53693f29ec6387dc28f02a894bf3
Gabriel <gabrielladeira8@gmail.com>
2023-08-20 21:46:05 -0300

    feat: should call verify with correct values

--- src/infra/cryptography/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter.ts
@@ -7,4 +7,8 @@ export class JwtAdapter implements Encrypter {
     const token = Jwt.sign({ id: plainText }, this.secret)
     return token
   }
+
+  async decrypt (plainText: string): Promise<string> {
+    return Jwt.verify(plainText, this.secret) as any
+  }
 }
