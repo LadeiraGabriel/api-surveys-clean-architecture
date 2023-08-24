2951159239ab6b7b62ad9167105dc0abee98126d
Gabriel <gabrielladeira8@gmail.com>
2023-08-23 21:46:12 -0300

    feat: should return list surveys on success

--- src/presentation/controllers/survey/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys-controller.ts
@@ -1,13 +1,11 @@
 import type { LoadSurveys } from '../../../domain/use-cases'
+import { ok } from '../../helpers/http-helper'
 import type { Controller, HttpResponse } from '../../protocols'
 
 export class LoadSurveysController implements Controller {
   constructor (private readonly loadSurveys: LoadSurveys) {}
   async handle (): Promise<HttpResponse> {
-    await this.loadSurveys.load()
-    return Promise.resolve({
-      statusCode: 200,
-      body: ''
-    })
+    const listSurveys = await this.loadSurveys.load()
+    return ok(listSurveys)
   }
 }
