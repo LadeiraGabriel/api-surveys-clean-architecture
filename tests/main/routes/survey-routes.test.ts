f3aabd747a75274492bbe8aee99753bbcb1315dc
Gabriel <gabrielladeira8@gmail.com>
2023-08-29 21:31:43 -0300

    test: should return 200 on success

--- tests/main/routes/survey-routes.test.ts
+++ tests/main/routes/survey-routes.test.ts
@@ -8,11 +8,13 @@ let app: Express
 
 beforeAll(async () => {
   app = await setupApp()
+  await prismaClientHelper.account.deleteMany({})
   await prismaClientHelper.anwer.deleteMany({})
   await prismaClientHelper.survey.deleteMany({})
 })
 
 afterAll(async () => {
+  await prismaClientHelper.account.deleteMany({})
   await prismaClientHelper.anwer.deleteMany({})
   await prismaClientHelper.survey.deleteMany({})
 })
@@ -43,6 +45,27 @@ describe('Survey Routes', () => {
           image: 'any_image'
         }]
       }).expect(204)
+    })
+  })
+
+  describe('Load Survey Route', () => {
+    test('Should return 200 on success', async () => {
+      let account = await prismaClientHelper.account.create({
+        data: {
+          name: 'any_name',
+          email: 'other_email@gmail.com',
+          password: '$2b$12$vE.nSc9qmclKapl15JxG/exfGpSHUzY2MCIjs/hLYlVNC3ITW.kt.'
+        }
+      })
+      account = await prismaClientHelper.account.update({
+        where: {
+          id: account.id
+        },
+        data: {
+          token: Jwt.sign({ id: account.id }, process.env.SECRET_KEY)
+        }
+      })
+      await request(app).get('/api/surveys').set('x-access-token', account.token).expect(200)
     })
   })
 })
