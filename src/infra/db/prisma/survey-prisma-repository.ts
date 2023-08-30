677d62534204537a4df8bd1fefe85fdb38d6c1f3
Gabriel <gabrielladeira8@gmail.com>
2023-08-29 21:00:13 -0300

    feat: should return list surveys on success

--- src/infra/db/prisma/survey-prisma-repository.ts
+++ src/infra/db/prisma/survey-prisma-repository.ts
@@ -1,7 +1,8 @@
 import type { AddSurveyRepository } from '../../../data/protocols/db/survey/add-survey-repository'
+import type { LoadSurveysRepository } from '../../../data/protocols/db/survey/load-surveys-repository'
 import { prismaClientHelper } from '../../helpers/prisma-client-helper'
 
-export class SurveyPrismaRepository implements AddSurveyRepository {
+export class SurveyPrismaRepository implements AddSurveyRepository, LoadSurveysRepository {
   async add (data: AddSurveyRepository.Params): AddSurveyRepository.Result {
     await prismaClientHelper.survey.create({
       data: {
@@ -13,5 +14,14 @@ export class SurveyPrismaRepository implements AddSurveyRepository {
         }
       }
     })
+  }
+
+  async loadSurveys (): Promise<LoadSurveysRepository.Result[]> {
+    const surveys = await prismaClientHelper.survey.findMany({
+      include: {
+        anwers: true
+      }
+    })
+    return surveys
   }
 }
