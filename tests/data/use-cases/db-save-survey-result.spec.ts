b18dd61d4dcfc319f4efd8d80e7fbac5eabec402
Gabriel <gabrielladeira8@gmail.com>
2023-09-07 21:14:10 -0300

    test: should call load survey result repository with correct values

--- tests/data/use-cases/db-save-survey-result.spec.ts
+++ tests/data/use-cases/db-save-survey-result.spec.ts
@@ -1,20 +1,25 @@
 import type { SaveSurveyResult } from '@/domain/use-cases/save-survey-result'
-import MockDate from 'mockdate'
-import { DbSaveSurveyResult } from '@/data/use-cases/db-save-survey-result'
 import type { SaveSurveyResultRepository } from '@/data/protocols/db/survey/save-survey-result-repository'
-import { makeSaveSurveyByIdRepositoryStub } from '../mocks/mock-save-survey-result'
+import type { LoadSurveyResultRepository } from '@/data/protocols/db/survey/load-survey-result-repository'
+import { DbSaveSurveyResult } from '@/data/use-cases/db-save-survey-result'
+import { makeSaveSurveyByIdRepositoryStub } from '@/tests/data/mocks/mock-save-survey-result'
+import { makeLoadSurveyResultRepositoryStub } from '@/tests/data/mocks/mock-load-survey-result'
+import MockDate from 'mockdate'
 
 type SutType = {
   sut: DbSaveSurveyResult
   saveSurveyResultRepository: SaveSurveyResultRepository
+  loadSurveyResultRepositoryStub: LoadSurveyResultRepository
 }
 
 const makeSut = (): SutType => {
+  const loadSurveyResultRepositoryStub = makeLoadSurveyResultRepositoryStub()
   const saveSurveyResultRepository = makeSaveSurveyByIdRepositoryStub()
-  const sut = new DbSaveSurveyResult(saveSurveyResultRepository)
+  const sut = new DbSaveSurveyResult(saveSurveyResultRepository, loadSurveyResultRepositoryStub)
   return {
     sut,
-    saveSurveyResultRepository
+    saveSurveyResultRepository,
+    loadSurveyResultRepositoryStub
   }
 }
 
@@ -46,5 +51,13 @@ describe('Db save survey result', () => {
     jest.spyOn(saveSurveyResultRepository, 'saveSurveyResult').mockReturnValueOnce(Promise.reject(new Error()))
     const result = sut.save(makeFakeRequest())
     await expect(result).rejects.toThrow()
+  })
+
+  test('should call load survey result repository with correct values', async () => {
+    const request = makeFakeRequest()
+    const { sut, loadSurveyResultRepositoryStub } = makeSut()
+    const spySaveResult = jest.spyOn(loadSurveyResultRepositoryStub, 'loadSurveyResult')
+    await sut.save(request)
+    expect(spySaveResult).toHaveBeenCalledWith(request.surveyId, request.accountId)
   })
 })
