400a7cfbb8974d4bb7f0340f04316d90416a5476
Gabriel <gabrielladeira8@gmail.com>
2023-11-11 15:34:58 -0300

    test: should return 403 if load anwers by survey return null

--- tests/presentation/controlller/save-survey-result-controller/save-survey-result-controller.spec.ts
+++ tests/presentation/controlller/save-survey-result-controller/save-survey-result-controller.spec.ts
@@ -1,5 +1,7 @@
 import type { LoadAnwersBySurvey } from '@/domain/use-cases/load-anwers-by-survey'
 import { SaveSurveyResultController } from '@/presentation/controllers/survey-result/save-survey-result-controller'
+import { InvalidParamError } from '@/presentation/errors'
+import { forbidden } from '@/presentation/helpers/http-helper'
 
 type SutType = {
   sut: SaveSurveyResultController
@@ -30,5 +32,14 @@ describe('Save survey result controller', () => {
     const loadSpy = jest.spyOn(loadAnwersBySurvey, 'loadAnwers')
     await sut.handle(httpRequest)
     expect(loadSpy).toHaveBeenCalledWith(httpRequest.surveyId)
+  })
+  test('should return 403 if load anwers by survey return null', async () => {
+    const httpRequest = {
+      surveyId: 'any_id'
+    }
+    const { sut, loadAnwersBySurvey } = makeSut()
+    jest.spyOn(loadAnwersBySurvey, 'loadAnwers').mockReturnValueOnce(null)
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(forbidden(new InvalidParamError('surveyId')))
   })
 })
