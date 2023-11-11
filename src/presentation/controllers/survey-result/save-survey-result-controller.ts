eb002714f75f45a1581057e37ff1b433f92cde8c
Gabriel <gabrielladeira8@gmail.com>
2023-11-11 15:34:30 -0300

    feat: should return 403 if load anwers by survey return null

--- src/presentation/controllers/survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result-controller.ts
@@ -1,11 +1,15 @@
 import type { LoadAnwersBySurvey } from '@/domain/use-cases/load-anwers-by-survey'
-import { ok } from '@/presentation/helpers/http-helper'
+import { InvalidParamError } from '@/presentation/errors'
+import { forbidden, ok } from '@/presentation/helpers/http-helper'
 import type { Controller, HttpResponse } from '@/presentation/protocols'
 
 export class SaveSurveyResultController implements Controller {
   constructor (private readonly loadAnwersBySurvey: LoadAnwersBySurvey) {}
   async handle (request: any): Promise<HttpResponse> {
-    await this.loadAnwersBySurvey.loadAnwers(request.surveyId)
+    const anwers = await this.loadAnwersBySurvey.loadAnwers(request.surveyId)
+    if (!anwers) {
+      return forbidden(new InvalidParamError('surveyId'))
+    }
     return Promise.resolve(ok({}))
   }
 }
