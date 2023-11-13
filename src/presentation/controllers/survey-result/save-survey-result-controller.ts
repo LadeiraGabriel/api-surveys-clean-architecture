3eb3d91947be31c6f005cbeb1379a3e2ac609de2
Gabriel <gabrielladeira8@gmail.com>
2023-11-12 21:45:00 -0300

    feat: should return 403 if anwer not is valid

--- src/presentation/controllers/survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result-controller.ts
@@ -7,8 +7,18 @@ export class SaveSurveyResultController implements Controller {
   constructor (private readonly loadAnwersBySurvey: LoadAnwersBySurvey) {}
   async handle (request: any): Promise<HttpResponse> {
     const anwers = await this.loadAnwersBySurvey.loadAnwers(request.surveyId)
-    if (!anwers) {
+    if (!anwers.length) {
       return forbidden(new InvalidParamError('surveyId'))
+    }
+    let anwerIsValid = false
+    for (const anwer of anwers) {
+      if (anwer === request.anwer) {
+        anwerIsValid = true
+        break
+      }
+    }
+    if (!anwerIsValid) {
+      return forbidden(new InvalidParamError('anwer'))
     }
     return Promise.resolve(ok({}))
   }
