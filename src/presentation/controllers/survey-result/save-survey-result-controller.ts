a2747c02e42c5421b207f89c76371d9c385559e8
Gabriel <gabrielladeira8@gmail.com>
2023-11-13 22:21:32 -0300

    feat: should return 500 if save survey result fails

--- src/presentation/controllers/survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result-controller.ts
@@ -1,32 +1,36 @@
 import type { SaveSurveyResult } from './../../../domain/use-cases/save-survey-result'
 import type { LoadAnwersBySurvey } from '@/domain/use-cases/load-anwers-by-survey'
 import { InvalidParamError } from '@/presentation/errors'
-import { forbidden, ok } from '@/presentation/helpers/http-helper'
+import { forbidden, ok, serverError } from '@/presentation/helpers/http-helper'
 import type { Controller, HttpResponse } from '@/presentation/protocols'
 
 export class SaveSurveyResultController implements Controller {
-  constructor (private readonly loadAnwersBySurvey: LoadAnwersBySurvey, private readonly saveSurveyResult: SaveSurveyResult) {}
+  constructor (private readonly loadAnwersBySurvey: LoadAnwersBySurvey, private readonly saveSurveyResult: SaveSurveyResult) { }
   async handle (request: any): Promise<HttpResponse> {
-    const anwers = await this.loadAnwersBySurvey.loadAnwers(request.surveyId)
-    if (!anwers.length) {
-      return forbidden(new InvalidParamError('surveyId'))
-    }
-    let anwerIsValid = false
-    for (const anwer of anwers) {
-      if (anwer === request.anwer) {
-        anwerIsValid = true
-        break
+    try {
+      const anwers = await this.loadAnwersBySurvey.loadAnwers(request.surveyId)
+      if (!anwers.length) {
+        return forbidden(new InvalidParamError('surveyId'))
       }
+      let anwerIsValid = false
+      for (const anwer of anwers) {
+        if (anwer === request.anwer) {
+          anwerIsValid = true
+          break
+        }
+      }
+      if (!anwerIsValid) {
+        return forbidden(new InvalidParamError('anwer'))
+      }
+      await this.saveSurveyResult.save({
+        accountId: request.accountId,
+        surveyId: request.surveyId,
+        anwer: request.anwer,
+        date: new Date()
+      })
+      return Promise.resolve(ok({}))
+    } catch (error) {
+      return serverError(error)
     }
-    if (!anwerIsValid) {
-      return forbidden(new InvalidParamError('anwer'))
-    }
-    await this.saveSurveyResult.save({
-      accountId: request.accountId,
-      surveyId: request.surveyId,
-      anwer: request.anwer,
-      date: new Date()
-    })
-    return Promise.resolve(ok({}))
   }
 }
