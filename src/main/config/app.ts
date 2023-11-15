c80d44aec208fa95776927fd57566e3d8d2be3ce
Gabriel <gabrielladeira8@gmail.com>
2023-11-15 15:26:45 -0300

    feat: add swagger api

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,9 +1,11 @@
 import type { Express } from 'express'
+import setupSwagger from './config-swagger'
 import setupMiddleware from './middleware'
 import setupRoutes from './router'
 import express from 'express'
 export const setupApp = async (): Promise<Express> => {
   const app = express()
+  setupSwagger(app)
   setupMiddleware(app)
   setupRoutes(app)
   return app
