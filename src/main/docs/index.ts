acf3783a5ac1a46bb926e1b3b6e131673098fc59
Gabriel <gabrielladeira8@gmail.com>
2023-11-15 16:47:21 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,25 @@
+import { loginPath } from './paths/login-path'
+import { accountSchema } from '@/main/docs/schemas/account-schema'
+import { loginParamsSchema } from '@/main/docs/schemas/login-params-schema'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node API',
     description: 'API de enquetes construida em node js com foco no estudo de arquitetura limpa e TDD',
     version: '1.0.0'
+  },
+  servers: [{
+    url: '/api'
+  }],
+  tags: [{
+    name: 'Login'
+  }],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema
   }
 }
