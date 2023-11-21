abd0f6a8686f7dda472e7a31ea9b9b323278277d
Gabriel <gabrielladeira8@gmail.com>
2023-11-20 21:45:44 -0300

    test: should return survey result with values null if survey result return null

--- tests/data/use-cases/db-load-survey-result.spec.ts
+++ tests/data/use-cases/db-load-survey-result.spec.ts
@@ -27,4 +27,28 @@ describe('Db load survey result', () => {
     await sut.load(params)
     expect(spyloadResult).toHaveBeenCalledWith(params.surveyId, params.accountId)
   })
+
+  test('should return survey result with values null if survey result return null', async () => {
+    const params = {
+      accountId: 'any_accountId',
+      surveyId: 'any_surveyId'
+    }
+    const { sut, loadSurveyResultRepository } = makeSut()
+    jest.spyOn(loadSurveyResultRepository, 'loadBySurveyId').mockReturnValueOnce(Promise.resolve(null))
+    const loadResult = await sut.load(params)
+    expect(loadResult).toEqual({
+      surveyId: 'any_surveyid',
+      question: 'any_question',
+      anwers: [
+        {
+          image: 'any_image',
+          anwer: 'any_anwer',
+          count: 0,
+          percent: 0,
+          isCurrentAccountAnwer: false
+        }
+      ],
+      date: new Date()
+    })
+  })
 })
