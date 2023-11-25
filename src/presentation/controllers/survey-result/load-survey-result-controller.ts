906d3dcaa7014588adb5cd1141baba95f6e812b9
Gabriel <gabrielladeira8@gmail.com>
2023-11-25 12:52:29 -0300

    feat: should return 403 if check survey by id return false

--- src/presentation/controllers/survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result-controller.ts
@@ -1,11 +1,16 @@
 import type { CheckSurveyById } from '@/domain/use-cases/check-survey-by-id'
+import { forbidden } from '@/presentation/helpers/http-helper'
+import { InvalidParamError } from '@/presentation/errors'
 import type { Controller, HttpResponse } from '@/presentation/protocols'
 
 export class LoadSurveyResultController implements Controller {
   constructor (private readonly checkSurveyById: CheckSurveyById) { }
   async handle (request: any): Promise<HttpResponse> {
     const { surveyId } = request
-    await this.checkSurveyById.checkById(surveyId)
+    const checkSurveyId = await this.checkSurveyById.checkById(surveyId)
+    if (!checkSurveyId) {
+      return forbidden(new InvalidParamError('surveyId'))
+    }
     return Promise.resolve({
       statusCode: 200,
       body: {}
