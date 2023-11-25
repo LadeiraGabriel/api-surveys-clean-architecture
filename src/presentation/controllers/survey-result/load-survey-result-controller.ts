c73d9af3572442519c1eab5495c02cb4535ececc
Gabriel <gabrielladeira8@gmail.com>
2023-11-25 13:44:05 -0300

    feat: should call load survey result with correct values

--- src/presentation/controllers/survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result-controller.ts
@@ -1,17 +1,19 @@
 import type { CheckSurveyById } from '@/domain/use-cases/check-survey-by-id'
+import type { LoadSurveyResult } from '@/domain/use-cases/load-survey-result'
 import { forbidden, serverError } from '@/presentation/helpers/http-helper'
 import { InvalidParamError } from '@/presentation/errors'
 import type { Controller, HttpResponse } from '@/presentation/protocols'
 
 export class LoadSurveyResultController implements Controller {
-  constructor (private readonly checkSurveyById: CheckSurveyById) { }
+  constructor (private readonly checkSurveyById: CheckSurveyById, private readonly loadSurveyResult: LoadSurveyResult) { }
   async handle (request: any): Promise<HttpResponse> {
     try {
-      const { surveyId } = request
+      const { surveyId, accountId } = request
       const checkSurveyId = await this.checkSurveyById.checkById(surveyId)
       if (!checkSurveyId) {
         return forbidden(new InvalidParamError('surveyId'))
       }
+      await this.loadSurveyResult.load(accountId, surveyId)
       return Promise.resolve({
         statusCode: 200,
         body: {}
