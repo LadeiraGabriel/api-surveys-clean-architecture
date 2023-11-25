cfd5a28ea56c8a2bed5704ab4651c430d719927c
Gabriel <gabrielladeira8@gmail.com>
2023-11-25 13:19:56 -0300

    test: should return 500 if check survey by id throws

--- tests/presentation/controlller/load-survey-result-controller/load-survey-result-controller.spec.ts
+++ tests/presentation/controlller/load-survey-result-controller/load-survey-result-controller.spec.ts
@@ -2,6 +2,8 @@ import MockDate from 'mockdate'
 import type { CheckSurveyById } from '@/domain/use-cases/check-survey-by-id'
 import { mockCheckSurveyById } from '../../mocks/mock-survey'
 import { LoadSurveyResultController } from '@/presentation/controllers/survey-result/load-survey-result-controller'
+import { forbidden, serverError } from '@/presentation/helpers/http-helper'
+import { InvalidParamError } from '@/presentation/errors'
 
 beforeAll(() => {
   MockDate.set(new Date())
@@ -24,8 +26,8 @@ const makeSut = (): SutType => {
   }
 }
 
-describe('Save survey result controller', () => {
-  test('should call load anwers by survey with correct value', async () => {
+describe('Load survey result controller', () => {
+  test('should call check survey by id with correct value', async () => {
     const httpRequest = {
       surveyId: 'any_id'
     }
@@ -33,5 +35,25 @@ describe('Save survey result controller', () => {
     const checkSpy = jest.spyOn(checkSurveyById, 'checkById')
     await sut.handle(httpRequest)
     expect(checkSpy).toHaveBeenCalledWith(httpRequest.surveyId)
+  })
+
+  test('should return 403 if check survey by id return false', async () => {
+    const httpRequest = {
+      surveyId: 'any_id'
+    }
+    const { sut, checkSurveyById } = makeSut()
+    jest.spyOn(checkSurveyById, 'checkById').mockResolvedValueOnce(Promise.resolve(false))
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(forbidden(new InvalidParamError('surveyId')))
+  })
+
+  test('should return 500 if check survey by id throws', async () => {
+    const httpRequest = {
+      surveyId: 'any_id'
+    }
+    const { sut, checkSurveyById } = makeSut()
+    jest.spyOn(checkSurveyById, 'checkById').mockResolvedValueOnce(Promise.reject(new Error()))
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 })
