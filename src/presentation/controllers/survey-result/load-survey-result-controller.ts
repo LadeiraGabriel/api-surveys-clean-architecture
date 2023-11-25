e53b44b52ab43dc673e5dfda8e165bc2de159913
Gabriel <gabrielladeira8@gmail.com>
2023-11-25 13:19:15 -0300

    feat: should return 500 if check survey by id throws

--- src/presentation/controllers/survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result-controller.ts
@@ -1,19 +1,23 @@
 import type { CheckSurveyById } from '@/domain/use-cases/check-survey-by-id'
-import { forbidden } from '@/presentation/helpers/http-helper'
+import { forbidden, serverError } from '@/presentation/helpers/http-helper'
 import { InvalidParamError } from '@/presentation/errors'
 import type { Controller, HttpResponse } from '@/presentation/protocols'
 
 export class LoadSurveyResultController implements Controller {
   constructor (private readonly checkSurveyById: CheckSurveyById) { }
   async handle (request: any): Promise<HttpResponse> {
-    const { surveyId } = request
-    const checkSurveyId = await this.checkSurveyById.checkById(surveyId)
-    if (!checkSurveyId) {
-      return forbidden(new InvalidParamError('surveyId'))
+    try {
+      const { surveyId } = request
+      const checkSurveyId = await this.checkSurveyById.checkById(surveyId)
+      if (!checkSurveyId) {
+        return forbidden(new InvalidParamError('surveyId'))
+      }
+      return Promise.resolve({
+        statusCode: 200,
+        body: {}
+      })
+    } catch (error) {
+      return serverError(error)
     }
-    return Promise.resolve({
-      statusCode: 200,
-      body: {}
-    })
   }
 }
