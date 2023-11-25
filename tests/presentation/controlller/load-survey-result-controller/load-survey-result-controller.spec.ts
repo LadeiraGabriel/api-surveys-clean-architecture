60655c9086e9445eaccf4fda8f6376e2408c44b8
Gabriel <gabrielladeira8@gmail.com>
2023-11-25 13:44:32 -0300

    test: should call load survey result with correct values

--- tests/presentation/controlller/load-survey-result-controller/load-survey-result-controller.spec.ts
+++ tests/presentation/controlller/load-survey-result-controller/load-survey-result-controller.spec.ts
@@ -4,6 +4,8 @@ import { mockCheckSurveyById } from '../../mocks/mock-survey'
 import { LoadSurveyResultController } from '@/presentation/controllers/survey-result/load-survey-result-controller'
 import { forbidden, serverError } from '@/presentation/helpers/http-helper'
 import { InvalidParamError } from '@/presentation/errors'
+import type { LoadSurveyResult } from '@/domain/use-cases/load-survey-result'
+import { mockLoadSurveyResultStub } from '../../mocks/mock-survey-result'
 
 beforeAll(() => {
   MockDate.set(new Date())
@@ -15,14 +17,17 @@ afterAll(() => {
 type SutType = {
   sut: LoadSurveyResultController
   checkSurveyById: CheckSurveyById
+  loadSurveyResultStub: LoadSurveyResult
 }
 
 const makeSut = (): SutType => {
   const checkSurveyById = mockCheckSurveyById()
-  const sut = new LoadSurveyResultController(checkSurveyById)
+  const loadSurveyResultStub = mockLoadSurveyResultStub()
+  const sut = new LoadSurveyResultController(checkSurveyById, loadSurveyResultStub)
   return {
     sut,
-    checkSurveyById
+    checkSurveyById,
+    loadSurveyResultStub
   }
 }
 
@@ -55,5 +60,16 @@ describe('Load survey result controller', () => {
     jest.spyOn(checkSurveyById, 'checkById').mockResolvedValueOnce(Promise.reject(new Error()))
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  test('should call load survey result with correct value', async () => {
+    const httpRequest = {
+      surveyId: 'any_id',
+      accountId: 'any_id'
+    }
+    const { sut, loadSurveyResultStub } = makeSut()
+    const loadSpy = jest.spyOn(loadSurveyResultStub, 'load')
+    await sut.handle(httpRequest)
+    expect(loadSpy).toHaveBeenCalledWith(httpRequest.surveyId, httpRequest.accountId)
   })
 })
--- tests/presentation/mocks/mock-survey-result.ts
+++ tests/presentation/mocks/mock-survey-result.ts
@@ -1,4 +1,5 @@
 import type { LoadAnwersBySurvey } from '@/domain/use-cases/load-anwers-by-survey'
+import type { LoadSurveyResult } from '@/domain/use-cases/load-survey-result'
 import type { SaveSurveyResult } from '@/domain/use-cases/save-survey-result'
 
 export class LoadAnwersBySurveyStub implements LoadAnwersBySurvey {
@@ -24,6 +25,29 @@ export class SaveSurveyResultStub implements SaveSurveyResult {
       date: new Date()
     })
   }
+}
+
+export class LoadSurveyResultStub implements LoadSurveyResult {
+  async load (surveyId: string, accountId: string): Promise<LoadSurveyResult.Result> {
+    return Promise.resolve({
+      surveyId: 'any_id',
+      question: 'any_question',
+      anwers: [
+        {
+          image: 'any_image',
+          anwer: 'any_anwer',
+          count: 100,
+          percent: 50,
+          isCurrentAccountAnwer: true
+        }
+      ],
+      date: new Date()
+    })
+  }
+}
+
+export const mockLoadSurveyResultStub = (): LoadSurveyResult => {
+  return new LoadSurveyResultStub()
 }
 
 export const mockSaveSurveyResultStub = (): SaveSurveyResult => {
