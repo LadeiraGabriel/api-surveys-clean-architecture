be248212e51b961a08ed2b8bfcdad40215073992
Gabriel <gabrielladeira8@gmail.com>
2023-11-26 22:36:06 -0300

    docs: add load survey result api

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -50,5 +50,48 @@ export const surveyResultPath = {
         $ref: '#/components/serverError'
       }
     }
+  },
+  get: {
+    security: [{
+      apiKeyAuth: []
+    }],
+    tags: ['Surveys'],
+    summary: 'API para mostrar os resultado de uma enquete',
+    parameters:
+      [
+        {
+          in: 'path',
+          name: 'surveyId',
+          required: true,
+          schema: {
+            type: 'string'
+          },
+          description: 'The survey ID'
+        }
+      ],
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+      400: {
+        $ref: '#/components/badRequest'
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
   }
 }
