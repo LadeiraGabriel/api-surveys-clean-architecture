fc751a1a9bf40ab7955209464e4ef035646c2e6d
Gabriel <gabrielladeira8@gmail.com>
2023-11-26 21:24:08 -0300

    test: add router survey result, should return 200 on success

--- tests/main/routes/surveyResultRoutes.test.ts
+++ tests/main/routes/surveyResultRoutes.test.ts
@@ -13,6 +13,19 @@ beforeAll(async () => {
   await prismaClientHelper.survey.deleteMany({})
 })
 
+beforeEach(async () => {
+  app = await setupApp()
+  await prismaClientHelper.account.deleteMany({})
+  await prismaClientHelper.anwer.deleteMany({})
+  await prismaClientHelper.survey.deleteMany({})
+})
+
+afterEach(async () => {
+  await prismaClientHelper.account.deleteMany({})
+  await prismaClientHelper.anwer.deleteMany({})
+  await prismaClientHelper.survey.deleteMany({})
+})
+
 afterAll(async () => {
   await prismaClientHelper.account.deleteMany({})
   await prismaClientHelper.anwer.deleteMany({})
@@ -58,9 +71,53 @@ describe('Survey Routes', () => {
         }
       })
       await request(app).put(`/api/surveys/${survey.id}/results`).set('x-access-token', account.token).send({
-        surveyId: survey.id,
         anwer: 'other_anwer'
       }).expect(200)
+    })
+  })
+
+  describe('Load Survey Result Route', () => {
+    test('Should return 200 on success', async () => {
+      const survey = await prismaClientHelper.survey.create({
+        data: {
+          question: 'any_question',
+          date: new Date(),
+          anwers: {
+            createMany: {
+              data: [
+                {
+                  anwer: 'any_anwer',
+                  image: 'any_iamge'
+                },
+                {
+                  anwer: 'other_anwer',
+                  image: 'other_image'
+                }
+              ]
+            }
+          }
+        }
+      })
+      let account = await prismaClientHelper.account.create({
+        data: {
+          name: 'any_name',
+          email: 'any_email@gmail.com',
+          password: '$2b$12$vE.nSc9qmclKapl15JxG/exfGpSHUzY2MCIjs/hLYlVNC3ITW.kt.'
+        }
+      })
+      account = await prismaClientHelper.account.update({
+        where: {
+          id: account.id
+        },
+        data: {
+          token: Jwt.sign({ id: account.id }, process.env.SECRET_KEY)
+        }
+      })
+      await request(app)
+        .get(`/api/surveys/${survey.id}/results`)
+        .set('x-access-token', account.token)
+        .send({})
+        .expect(200)
     })
   })
 })
