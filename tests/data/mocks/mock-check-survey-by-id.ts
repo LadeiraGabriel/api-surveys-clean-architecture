335a8e7ef6c1356118b5a4a7e00ec2d902d152a8
Gabriel <gabrielladeira8@gmail.com>
2023-11-30 21:44:11 -0300

    refactor: change double test stub to spy in mock check survey by id repository

--- tests/data/mocks/mock-check-survey-by-id.ts
+++ tests/data/mocks/mock-check-survey-by-id.ts
@@ -1,11 +1,14 @@
 import type { CheckSurveyByIdRepository } from '@/data/protocols/db/survey/check-survey-by-id-repository'
 
-class CheckSurveyByIdRepositoryStub implements CheckSurveyByIdRepository {
+export class CheckSurveyByIdRepositorySpy implements CheckSurveyByIdRepository {
+  id: string
+  result: CheckSurveyByIdRepository.Result = true
   async checkById (id: string): Promise<CheckSurveyByIdRepository.Result> {
-    return Promise.resolve(true)
+    this.id = id
+    return this.result
   }
 }
 
-export const makeCheckSurveyByIdRepositoryStub = (): CheckSurveyByIdRepository => {
-  return new CheckSurveyByIdRepositoryStub()
+export const makeCheckSurveyByIdRepositoryStub = (): CheckSurveyByIdRepositorySpy => {
+  return new CheckSurveyByIdRepositorySpy()
 }
