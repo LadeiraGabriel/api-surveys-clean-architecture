bdcd6bae41a4ac778bf0181f2dd8603e3ffb634c
Gabriel <gabrielladeira8@gmail.com>
2023-12-03 22:43:04 -0300

    feat: add api static path to images

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,10 +1,12 @@
 import type { Express } from 'express'
+import setupStaticPath from './static'
 import setupSwagger from './config-swagger'
 import setupMiddleware from './middleware'
 import setupRoutes from './router'
 import express from 'express'
 export const setupApp = async (): Promise<Express> => {
   const app = express()
+  setupStaticPath(app)
   setupSwagger(app)
   setupMiddleware(app)
   setupRoutes(app)
